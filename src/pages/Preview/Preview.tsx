4a64fa0f200f03cae07ad4e31dc12f1c27d07316
DominicWrege <dominic.wrege001@stud.fh-dortmund.de>
2021-07-07 11:25:51 +0200

    preview go back btn works

--- src/pages/Preview/Preview.tsx
+++ src/pages/Preview/Preview.tsx
@@ -12,128 +12,130 @@ import category from "../../functions/category";
 const { Title, Text } = Typography;
 
 async function getPreview(url: string): Promise<PreviewJson> {
-  const body: FormBody = {
-    feedUrl: url,
-  };
-  const resp = await http.post(
-    `${API_URL}/feed/preview`,
-    body,
-    http.WithCredentials.Yes
-  );
-  return resp.json();
+	const body: FormBody = {
+		feedUrl: url,
+	};
+	const resp = await http.post(
+		`${API_URL}/feed/preview`,
+		body,
+		http.WithCredentials.Yes
+	);
+	return resp.json();
 }
 
 const Preview: React.FC = () => {
-  const [feed, setFeed] = useState<FeedEpisodeModel | null>(null);
-  const [feedExist, setFeedExists] = useState<boolean>(false);
-  const setLocation = useLocation()[1];
-  const [feedUrl, setFeedUrl] = useState<string | null>(null);
-  const [error, setError] = useState<ApiError | null>(null);
+	const [feed, setFeed] = useState<FeedEpisodeModel | null>(null);
+	const [feedExist, setFeedExists] = useState<boolean>(false);
+	const setLocation = useLocation()[1];
+	const [feedUrl, setFeedUrl] = useState<string | null>(null);
+	const [error, setError] = useState<ApiError | null>(null);
 
-  const [saveLoading, setSaveLoading] = useState(false);
+	const [saveLoading, setSaveLoading] = useState(false);
 
-  const handleSaveFeed = async (): Promise<void> => {
-    if (feedUrl) {
-      setSaveLoading(true);
-      const body: FormBody = {
-        feedUrl: feedUrl,
-      };
-      try {
-        await http.post(`${API_URL}/feed/new`, body, http.WithCredentials.Yes);
-        setSaveLoading(false);
-        setLocation("/new/done");
-      } catch (err: http.HttpError | any) {
-        setSaveLoading(false);
-        console.log(err.c);
-        setError(err.json);
-      }
-    }
-  };
+	const handleSaveFeed = async (): Promise<void> => {
+		if (feedUrl) {
+			setSaveLoading(true);
+			const body: FormBody = {
+				feedUrl: feedUrl,
+			};
+			try {
+				await http.post(`${API_URL}/feed/new`, body, http.WithCredentials.Yes);
+				setSaveLoading(false);
+				setLocation("/new/done");
+			} catch (err: http.HttpError | any) {
+				setSaveLoading(false);
+				console.log(err.c);
+				setError(err.json);
+			}
+		}
+	};
 
-  const renderActions = (): JSX.Element | null => {
-    const save_btn = feedExist ? (
-      <Button disabled type="primary">
-        Save Feed
-      </Button>
-    ) : (
-      <Button type="primary" onClick={handleSaveFeed} loading={saveLoading}>
-        Save Feed
-      </Button>
-    );
+	const renderActions = (): JSX.Element | null => {
+		const save_btn = feedExist ? (
+			<Button disabled type="primary">
+				Save Feed
+			</Button>
+		) : (
+			<Button type="primary" onClick={handleSaveFeed} loading={saveLoading}>
+				Save Feed
+			</Button>
+		);
 
-    const preText = feedExist ? (
-      <Text type="danger" key="extras541">
-        Feed already exists
-      </Text>
-    ) : null;
-    return (
-      <div className="Preview-card-extras" key="extras771">
-        {preText}
-        {save_btn}
-      </div>
-    );
-  };
+		const preText = feedExist ? (
+			<Text type="danger" key="extras541">
+				Feed already exists
+			</Text>
+		) : null;
+		return (
+			<div className="Preview-card-extras" key="extras771">
+				{preText}
+				{save_btn}
+			</div>
+		);
+	};
 
-  const init = useCallback(async () => {
-    const params = new URLSearchParams(window.location.search);
-    const paramsUrl = params.get("url");
+	const init = useCallback(async () => {
+		const params = new URLSearchParams(window.location.search);
+		const paramsUrl = params.get("url");
 
-    if (paramsUrl) {
-      setFeedUrl(paramsUrl);
-      try {
-        const preview = await getPreview(paramsUrl);
-        setFeed({
-          ...preview.feed,
-          categories: category.castCategories(preview.feed.categories),
-        });
-        setFeedExists(preview.exists);
-      } catch (err: http.HttpError | any) {
-        setError(err.json);
-      }
-    }
-  }, []);
+		if (paramsUrl) {
+			setFeedUrl(paramsUrl);
+			try {
+				const preview = await getPreview(paramsUrl);
+				setFeed({
+					...preview.feed,
+					categories: category.castCategories(preview.feed.categories),
+				});
+				setFeedExists(preview.exists);
+			} catch (err: http.HttpError | any) {
+				setError(err.json);
+			}
+		}
+	}, []);
 
-  useEffect(() => {
-    init();
-  }, [init]);
+	useEffect(() => {
+		init();
+	}, [init]);
 
-  if (error) {
-    return (
-      <Card>
-        <Result
-          status="error"
-          title={<div>Error Code: {error.statusCode}</div>}
-          subTitle={error.message}
-          extra={
-            <Button type="primary" key="extra-b876565">
-              Go Back
-            </Button>
-          }
-        />
-      </Card>
-    );
-  }
+	const goBack = () => setLocation("/new/feed");
 
-  return (
-    <div className="Preview">
-      <Card
-        id="Preview-card"
-        title={
-          <div className="Preview-card-title">
-            <Button
-              type="text"
-              icon={<ArrowLeftOutlined />}
-              onClick={() => setLocation("/new/feed")}
-            ></Button>
-            <Title level={4}>Preview</Title>
-          </div>
-        }
-        extra={[renderActions()]}
-      >
-        <FeedDetail feed={feed} loadingFeed={feed === null} />
-      </Card>
-    </div>
-  );
+	if (error) {
+		return (
+			<Card>
+				<Result
+					status="error"
+					title={<div>Error Code: {error.statusCode}</div>}
+					subTitle={error.message}
+					extra={
+						<Button type="primary" onClick={goBack} key="extra-b876565">
+							Go Back
+						</Button>
+					}
+				/>
+			</Card>
+		);
+	}
+
+	return (
+		<div className="Preview">
+			<Card
+				id="Preview-card"
+				title={
+					<div className="Preview-card-title">
+						<Button
+							type="text"
+							icon={<ArrowLeftOutlined />}
+							onClick={goBack}
+						></Button>
+						<Title level={4}>Preview</Title>
+					</div>
+				}
+				extra={[renderActions()]}
+			>
+				<FeedDetail feed={feed} loadingFeed={feed === null} />
+			</Card>
+		</div>
+	);
 };
 
 export default Preview;
